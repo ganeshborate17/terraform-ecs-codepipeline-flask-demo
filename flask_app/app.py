38a98f732d17e5071b1acf9d5cadd8aeb62d7022
root <root@ip-172-31-2-94.ap-south-1.compute.internal>
2023-03-20 19:35:55 +0000

    full setup

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Yayy, we have the 4th version of our flask application"
+    return "Yayy, HELLO WORLD 1"
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
